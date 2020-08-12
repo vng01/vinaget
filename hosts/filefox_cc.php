c00742898825f52c2438a888bbd00dfb884b3344
vng01 <69549505+vng01@users.noreply.github.com>
2020-08-12 02:08:53 +0200

    Update filefox_cc.php

--- hosts/filefox_cc.php
+++ hosts/filefox_cc.php
@@ -2,47 +2,47 @@
 
 class dl_filefox_cc extends Download
 {
+	public function CheckAcc($cookie)
+	{
+		$data = $this->lib->curl("https://filefox.cc/profile", $cookie, "");
+		
+		if (stristr($data, 'premium account expire'))
+		{
+			return array(true, "Valid until: " . $this->lib->cut_str(end(explode("Premium account expires", $data)), '<a href="/premium">', '</') . "<br>Traffic restant: ".$this->lib->cut_str(explode("Traffic Available", $data)[1], '<div style="width', '</div>'));
+		}
+		elseif (stristr($data, 'Free member'))
+		{
+			return array(false, "Error getting the link from this account.");
+		} 
+		return array(false, "Error getting the link from this account.");
+	}
 
-    public function CheckAcc($cookie)
-    {
-        $data = $this->lib->curl("https://filefox.cc/profile", $cookie, "");
-        if (stristr($data, 'Premium account expires')) {
-            return array(true, "Until " . $this->lib->cut_str($this->lib->cut_str($data, 'Premium account expires', '</div>'), '<a href="/premium">', '</a>'));
-        } else if (stristr($data, 'Username / Email') && !stristr($data, 'Premium account expires')) {
-            return array(false, "accfree");
-        } else {
-            return array(false, "accinvalid");
-        }
-    }
+	public function Login($user, $pass)
+	{
+		$data = $this->lib->curl("https://filefox.cc/login", "lang=english;", "email={$user}&password={$pass}&op=login&redirect=&rand=");
+		$cookie = "lang=english;{$this->lib->GetCookies($data)}";
+		 return array(true, $cookie);
+	}
 
-    public function Login($user, $pass)
-    {
-        $data = $this->lib->curl("https://filefox.cc/login", "", "email={$user}&password={$pass}&op=login");
-        $cookie = $this->lib->GetCookies($data);
-        return array(true, $cookie);
-    }
+	public function Leech($url)
+	{
+		$data   = $this->lib->curl($url,$this->lib->cookie,"");
+		$post	= $this->parseForm($this->lib->cut_str($data, '<form', '</form>'));
+		$data   = $this->lib->curl($url,$this->lib->cookie,$post);
+		if (stristr($data, 'You have reached your download limit')) $this->error("quota atteint", true, false);
+		$link   = $this->lib->cut_str($data,'<a class="btn btn-default" href="','"');
+		
+		if($link !="") return str_replace(" ","_",$link);
+		return false;
+	}
 
-    public function Leech($url)
-    {
-        $data = $this->lib->curl($url, $this->lib->cookie, "");
-        if (!stristr($data, 'file-name') && !stristr($data, 'Download File')) {
-            $this->error("dead", true, false, 2);
-        } else {
-            $post = $this->parseForm($this->lib->cut_str($data, 'name="F1">', '</form>'));
-            $data = $this->lib->curl($url, $this->lib->cookie, $post);
-            if (preg_match('/<a class="btn btn-default" href="(.*?)">/', $data, $match)) {
-                return trim($match[1]);
-            }
-        }
-
-        return false;
-    }
 }
 
 /*
- * Open Source Project
- * New Vinaget by LTT
- * Version: 3.3 LTS
- * Filefox.cc Download Plugin
- * Date: 30.05.2020
- */
+* Open Source Project
+* Vinaget by ..::[H]::..
+* Version: 2.7.0
+* Download Plugin by vng01 [12.8.2020]
+* Downloader Class By [FZ]
+*/
+?>
